17db405f4c5c9f59ffec434956ee038470bf11f1
tiago-sada <tiago@sada.so>
2021-01-28 14:40:26 -0800

    Task 2

--- index.js
+++ index.js
@@ -79,7 +79,27 @@ class Airplane {
   */
   
  class Car {
-    
+   constructor (model, milesPerGallon) {
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+   }
+
+   fill (gallons) {
+      this.tank = this.tank + gallons;
+   }
+
+   drive (distance) {
+    if (this.tank >= (distance / this.milesPerGallon )) {
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance / this.milesPerGallon);
+    } else {
+      this.odometer = this.odometer + (this.tank * this.milesPerGallon);
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+   }
   }
   
   /*
