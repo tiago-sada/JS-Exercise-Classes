ab63190bc3cfdfde0b645eb523023619a73b7003
tiago-sada <tiago@sada.so>
2021-01-28 14:59:06 -0800

    Task 5

--- index.js
+++ index.js
@@ -173,8 +173,34 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
+ class Student extends Lambdasian {
+     constructor (data) {
+        super(data);
+        this.previousBackground = data.previousBackground;
+        this.className = data.className;
+        this.favSubjects = data.favSubjects;
+     }
+
+     listSubjects () {
+       let result = "Loving";
+       for (let i = 0; i < this.favSubjects.length; i++) {
+         if (i < this.favSubjects.length - 1) {
+          result = result + ` ${this.favSubjects[i]},`;
+         } else {
+          result = result + ` ${this.favSubjects[i]}!`;
+         }
+       }
+      //  console.log(result);
+       return result;
+     }
+
+     PRAssignment(subject) {
+      return `${this.name} has submitted a PR for ${subject}`;
+     }
+
+     sprintChallenge (subject) {
+      return `${this.name} has begun a sprint challenge on ${subject}`;
+     }
  }
   
   /*
